7c948d20e98d4b16a4884752486beaceb2f8e02b
Mrunmayeeeee <mrunmayeebodade@gmail.com>
2026-08-20 16:06:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+let currentAudio = null;
+
+let btn = document.querySelectorAll(".btn");
+let stop = document.querySelector(".stop");
+
+btn.forEach(function(button) {
+
+    button.addEventListener("click", function() {
+
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+
+        let soundName = button.id;
+
+        currentAudio = new Audio("./sounds/" + soundName + ".mp3");
+
+        currentAudio.play();
+    });
+
+});
+
+stop.addEventListener("click", function() {
+
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+
+});
